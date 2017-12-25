1475660ae448041a37f380fb0a6a6379ce1cdbb3
paul patena <paulmichaelpatena@gmail.com>
2017-12-25 14:26:50 +1100

    initial revision

---
+++ Release/SnakeKeypadWithScore.cpp.d
@@ -0,0 +1,23 @@
+C:\Users\pmp\AppData\Local\Temp\VMicroBuilds\SnakeKeypadWithScore\uno\SnakeKeypadWithScore.cpp.o: \
+ C:\Users\pmp\AppData\Local\Temp\VMicroBuilds\SnakeKeypadWithScore\uno\SnakeKeypadWithScore.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\pmp\Documents\Arduino\libraries\Adafruit_NeoMatrix/Adafruit_NeoMatrix.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\pmp\Documents\Arduino\libraries\Adafruit_GFX_Library/Adafruit_GFX.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\pmp\Documents\Arduino\libraries\Adafruit_GFX_Library/gfxfont.h \
+ C:\Users\pmp\Documents\Arduino\libraries\Adafruit_NeoPixel/Adafruit_NeoPixel.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\LiquidCrystal\src/LiquidCrystal.h \
+ C:\Users\pmp\Documents\Arduino\libraries\Keypad\src/Keypad.h \
+ C:\Users\pmp\Documents\Arduino\libraries\Keypad\src/Key.h \
+ C:\Users\pmp\AppData\Local\Temp\VMicroBuilds\SnakeKeypadWithScore\uno\pixellist.h
